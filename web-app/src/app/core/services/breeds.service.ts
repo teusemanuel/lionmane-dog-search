2beb0aa14e7da976f8b58a0161f1fe854c2a5986
Mateus Emanuel Araujo <teusemanuel@gmail.com>
2022-01-03 15:19:15 -0300

    Create Home feature

--- web-app/src/app/core/services/breeds.service.ts
+++ web-app/src/app/core/services/breeds.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AppHttpClient } from '../http/app-http-client';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BreedsService {
+  constructor(private http: AppHttpClient) {}
 
-  constructor() { }
+  randonBreed(): Observable<Map<string, string[]>> {
+    return this.http.get('breeds/list/all/random', { responseRef: Map });
+  }
+
+  breeds(): Observable<Map<string, string[]>> {
+    return this.http.get('breeds/list/all', { responseRef: Map });
+  }
 }
